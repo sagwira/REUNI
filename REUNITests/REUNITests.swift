d36813853cd95da2a6fb0eb60dbc376f4c668d56
SG <Shaun.gwira@gmail.com>
2025-10-08 06:25:12 -0700

    Initial Commit

---
+++ REUNITests/REUNITests.swift
@@ -0,0 +1,17 @@
+//
+//  REUNITests.swift
+//  REUNITests
+//
+//  Created by rentamac on 10/8/25.
+//
+
+import Testing
+@testable import REUNI
+
+struct REUNITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ REUNIUITests/REUNIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  REUNIUITestsLaunchTests.swift
+//  REUNIUITests
+//
+//  Created by rentamac on 10/8/25.
+//
+
+import XCTest
+
+final class REUNIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
